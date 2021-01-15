5efde9f017d2f8e88624faabc374acde737eb455
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-15 12:57:47 +0530

    External JS and CSS added

---
+++ script.js
@@ -0,0 +1,6 @@
+function discord(){
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=799485174269739038&permissions=515136&scope=bot';
+}
+function github(){
+    window.location.href = 'https://github.com/girishrajani/WeatherBot';
+}
