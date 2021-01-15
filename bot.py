195dc8f9d4e9e7a80fb8a25dd34f9a877297182b
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-15 09:41:01 +0530

    Testing hello command

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
 import discord 
+import os
+
+from discord import message
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('#hello'):
+        await message.channel.send("HELLO!")
+
+# client.run(os.getenv("TOKEN"))
